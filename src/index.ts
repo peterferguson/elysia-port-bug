4536a4f74ba79111943ceb84c7c74b376db2527d
Peter Ferguson <peterferguson95@gmail.com>
2024-10-01 10:45:55 +0100

    removing listen works

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Elysia } from "elysia";
 
-export default new Elysia()
-	.get("/", () => "Hello Elysia")
-	.listen(process.env.PORT ?? 3000, ({ hostname, port }) => {
-		console.log(`🦊 Elysia is running at ${hostname}:${port}`);
-	});
+export default new Elysia().get("/", () => "Hello Elysia");
+// .listen(process.env.PORT ?? 3000, ({ hostname, port }) => {
+// 	console.log(`🦊 Elysia is running at ${hostname}:${port}`);
+// });
